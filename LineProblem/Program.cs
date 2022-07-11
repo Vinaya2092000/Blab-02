03cd586a8537a4349deb83cf519e046188bd63c8
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-07-11 19:16:42 +0530

    emp line

---
+++ LineProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LineProblem
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            LineEqual emp = new LineEqual();
+            emp.LineOne();
+            double ans1 = emp.LineOne();
+            double ans2 = emp.LineTwo();
+            if (ans1 == ans2)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+
+
+        }
+    }
+}
