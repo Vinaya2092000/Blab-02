8443002e60557bc97f5b7398d41eefbf8ffced8a
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-07-16 13:10:50 +0530

    Second

---
+++ Day5/BasicProgramA/Flipcoin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramA
+{
+    internal class Flipcoin
+    {
+        public void Flip_percent()
+        {
+            int Head = 0;
+            int Tail = 0;
+            Console.Write("Enter the number of flips: ");
+            int number_of_flips =  Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Day5/BasicProgramA/Power2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramA
+{
+    internal class Power2
+    {
+        public void Power_of_two()     // 2^n operation
+        {
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n < 0 || n >= 31 )
+            {
+                Console.Write("");
+            }
+        }
+    }
+}
